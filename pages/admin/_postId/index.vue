65a21b98c81eac61b1872ba45a383914b971f1dd
Marcos Vinnicius Martins <gronytzki@gmail.com>
2020-07-10 11:39:58 -0300

    Update project

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -1,27 +1,41 @@
 <template>
-    <div class="admn-post-page">
-        <section class="update-form">
-            <AdminPostForm :post="loadedPost" />
-        </section>
-    </div>
+  <div class="admn-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
 </template>
 
 <script>
-import AdminPostForm from '@/components/Admin/AdminPostForm'
+import AdminPostForm from "@/components/Admin/AdminPostForm";
 export default {
-    components:{
-        AdminPostForm
-    }, 
-    data(){
-        return{
-            loadedPost:{
-                author: 'Marcos do plural',
-                title: 'Este post é incrível',
-                content: 'Olha só como esse post é top da galaxya',
-                calendar: '10/07/2020',
-                thumbnailLink: 'https://wallpapercave.com/wp/Q9zm0x5.jpg'
-            }
-        }
-    }
-}
+  layout: "admin",
+  components: {
+    AdminPostForm
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: "Marcos do plural",
+        title: "Este post é incrível",
+        content: "Olha só como esse post é top da galaxya",
+        calendar: "10/07/2020",
+        thumbnailLink: "https://wallpapercave.com/wp/Q9zm0x5.jpg"
+      }
+    };
+  }
+};
 </script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -2,7 +2,7 @@
   <div class="admin-new-post-page">
     <section class="new-post-form">
       <AdminPostForm />
-     
+
     </section>
   </div>
 </template>
@@ -11,6 +11,7 @@
 import AdminPostForm from "@/components/Admin/AdminPostForm";
 
 export default {
+  layout: "admin",
   components: {
     AdminPostForm
   }
@@ -18,14 +19,14 @@ export default {
 </script>
 
 <style scoped>
+.new-post-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
   .new-post-form {
-    width: 90%;
-    margin: 20px auto;
+    width: 500px;
   }
-  
-  @media (min-width: 768px) {
-    .new-post-form {
-      width: 500px;
-    }
-  }
+}
 </style>
